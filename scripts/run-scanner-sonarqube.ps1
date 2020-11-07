a50c27aadcb141ffcbcfbf943cf886cbb230abf8
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-07 13:57:09 -0500

    Se actualizan los scripts, gitignore para omitir el coverage

--- scripts/run-scanner-sonarqube.ps1
+++ scripts/run-scanner-sonarqube.ps1
@@ -1,6 +1,15 @@
-dotnet test .\tests\CodeDesignPlus.Core.Test\CodeDesignPlus.Core.Test.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+cd ..
 
-dotnet sonarscanner begin /k:"CodeDesignPlus.Core" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="tests\CodeDesignPlus.Core.Test\coverage.opencover.xml" /d:sonar.coverage.exclusions="**Tests*.cs"
+# Vars
+$path = "tests\CodeDesignPlus.Core.Test"
+$project = "$path\CodeDesignPlus.Core.Test.csproj"
+$report = "$path\coverage.opencover.xml"
+
+
+# Run Sonnar Scanner
+dotnet test $project /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+
+dotnet sonarscanner begin /k:"CodeDesignPlus.Core" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="$report" /d:sonar.coverage.exclusions="**Test*.cs"
 
 dotnet build
 
