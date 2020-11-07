ab082e14d877bb61f31db0a0ceeeb9b25aacfe17
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-07 13:59:56 -0500

    Se ajustan los scripts

--- scripts/run-scanner-sonarqube.ps1
+++ scripts/run-scanner-sonarqube.ps1
@@ -5,7 +5,6 @@ $path = "tests\CodeDesignPlus.Core.Test"
 $project = "$path\CodeDesignPlus.Core.Test.csproj"
 $report = "$path\coverage.opencover.xml"
 
-
 # Run Sonnar Scanner
 dotnet test $project /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
